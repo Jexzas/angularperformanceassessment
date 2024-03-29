5bf4f57fcb3c2f9a3c72e761f6d1f0da120f6e13
Jexzas <jdrmail2007@yahoo.com>
2024-03-28 21:54:59 -0500

    ios testing

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,7 +12,7 @@ export class MapComponent {
   @Output() countrySelected = new EventEmitter<any>();
 
   touchSelect(e: Event): void {
-    console.log((e.target as HTMLElement));
+    console.log((e.target as HTMLElement).getAttribute("countryCode"));
     this.countrySelected.emit((e.target as HTMLElement).dataset.countryCode);
   }
 
